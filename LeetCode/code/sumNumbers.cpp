da76c7db84bcf6397332c8e1195204cfaeeb83bf
botao <mtaobo@yeah.net>
2020-10-29 12:29:01 +0800

    sumNumbers

--- LeetCode/code/sumNumbers.cpp
+++ LeetCode/code/sumNumbers.cpp
@@ -1,7 +1,7 @@
 /*
  * @Author: taobo
  * @Date: 2020-10-29 12:08:43
- * @LastEditTime: 2020-10-29 12:13:02
+ * @LastEditTime: 2020-10-29 12:27:29
  */
 #include <iostream>
 #include <algorithm>
@@ -27,8 +27,21 @@ public:
       res->right = constructTree(nums, 2*root+2);
       return res;
     }
+    void dfs(TreeNode* root, int& res, int now) {
+        if (root == nullptr) return;
+        if (!root->left && !root->right) {
+            now = now*10 + root->val;
+            res += now;
+            return;
+        }
+        now = now*10 + root->val;
+        dfs(root->left, res, now);
+        dfs(root->right, res, now);
+    }
     int sumNumbers(TreeNode* root) {
-        
+        int res = 0;
+        dfs(root, res, 0);
+        return res;
     }
 };
 
